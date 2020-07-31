1002ff5c02bd7135866232ab5395cb97cdaf0d72
TatchNicolas <TatchNicolas@users.noreply.github.com>
2020-07-31 14:16:16 +0900

    fmt

--- main.go
+++ main.go
@@ -7,32 +7,30 @@ import (
 	"strings"
 )
 
-
 func main() {
-        for _, pair := range os.Environ() {
-                key := strings.SplitN(pair, "=", 2)[0]
-                prefix := strings.SplitN(key, "_", 2)[0]
-                if prefix == "ENVECHO" {
-                        fmt.Printf("%s\n", pair)
-                }
-        }
-        host, ok := os.LookupEnv("ENVECHO_HOST")
-        if !ok {
-                host = "0.0.0.0"
-        }
-        port, ok := os.LookupEnv("ENVECHO_PORT")
-        if !ok {
-                port = "80"
-        }
-        message, ok := os.LookupEnv("ENVECHO_MESSAGE")
-        if !ok {
-                message = "Set a MESSAGE to configure response text"
-        }
-        address := fmt.Sprintf("%s:%s", host, port)
+	for _, pair := range os.Environ() {
+		key := strings.SplitN(pair, "=", 2)[0]
+		prefix := strings.SplitN(key, "_", 2)[0]
+		if prefix == "ENVECHO" {
+			fmt.Printf("%s\n", pair)
+		}
+	}
+	host, ok := os.LookupEnv("ENVECHO_HOST")
+	if !ok {
+		host = "0.0.0.0"
+	}
+	port, ok := os.LookupEnv("ENVECHO_PORT")
+	if !ok {
+		port = "80"
+	}
+	message, ok := os.LookupEnv("ENVECHO_MESSAGE")
+	if !ok {
+		message = "Set a MESSAGE to configure response text"
+	}
+	address := fmt.Sprintf("%s:%s", host, port)
 
-        // patternの仕組みは type ServeMuxのDoc読んだらわかる
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                fmt.Fprintf(w, message)
-        })
-        http.ListenAndServe(address, nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, message)
+	})
+	http.ListenAndServe(address, nil)
 }
